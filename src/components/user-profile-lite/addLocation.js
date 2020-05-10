4e0300e9fdda4a46720c68e990eccc9ed920955f
abdullahtariq <faabdullahtariq@gmail.com>
2020-05-10 21:41:07 +0500

    add slots

--- src/components/user-profile-lite/addLocation.js
+++ src/components/user-profile-lite/addLocation.js
@@ -22,26 +22,76 @@ export default class AddLocation extends Component {
         this.props= props;
         this.state ={
           location :'',
-          slots :''
+          slots :'',   
+          SlotsData: [],
         }
+        this.handleAddSocial = this.handleAddSocial.bind(this);
         this.handleLocationChange = this.handleLocationChange.bind(this); 
-        this.handleSlotChange = this.handleSlotChange.bind(this); 
-        this.handleLogin = this.handleLogin.bind(this)
+        this.handleStartChange = this.handleStartChange.bind(this)
+        this.handleEndChange = this.handleEndChange.bind(this)
+        this.handleRemoveSlot = this.handleRemoveSlot.bind(this)
 
     }
     handleLocationChange(event) {
       this.setState({location: event.target.value});
    }
-   handleSlotChange(event) {
-      this.setState({slots: event.target.value});
-   } 
-   handleLogin() {
-    console.log("EMail: " + this.state.location);
-    console.log("Password: " + this.state.slots);
-    this.setState({location:''})
-    this.setState({slots:0})
 
+
+handleAddSocial(event) {
+  this.setState({slots: event.target.value});
+     this.setState({
+       SlotsData: this.state.SlotsData.concat({startTime:"",endTime:"",isFilled:false})
+     });
+  }
+
+handleStartChange(ev,idx){
+ const newD = this.state.SlotsData.map((startTime, sidx)=>{
+   if(idx !== sidx) return startTime;
+   return {...startTime, startTime :ev.target.value}
+ })
+ this.setState({SlotsData: newD})
 }
+handleEndChange(ev,idx){
+  const newD = this.state.SlotsData.map((endTime, sidx)=>{
+    if(idx !== sidx) return endTime;
+    return {...endTime, endTime :ev.target.value}
+  })
+  this.setState({SlotsData: newD})
+ }
+
+handleSubmit = evt => {
+  evt.preventDefault();
+  const { location, SlotsData } = this.state;
+  console.log(this.state)
+
+  var data = {
+    slotsData : SlotsData,
+    name : location
+  }
+  debugger
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Accept': 'application/json' },
+    body: JSON.stringify(data)
+};
+fetch('https://parking-app-api.herokuapp.com/locations/addLocations', requestOptions)
+    .then(response => {
+      console.log(response)
+      debugger
+  })
+  .catch(error =>{
+      console.log(error)
+      debugger
+  })
+  this.setState({location:''})
+  this.setState({SlotsData:''})
+};
+
+handleRemoveSlot = idx => () => {
+  this.setState({
+    SlotsData: this.state.SlotsData.filter((s, sidx) => idx !== sidx)
+  });
+};
   render(){
       return(
   <Card small className="mb-4">
@@ -52,13 +102,13 @@ export default class AddLocation extends Component {
       <ListGroupItem className="p-3">
         <Row>
           <Col>
-            <Form>
+            <Form onSubmit={this.handleSubmit}>
               <Row form>
                 {/* Email */}
                 <Col md="6" className="form-group">
                   <label htmlFor="feEmail">Location</label>
                   <FormInput
-                    type="email"
+                    type="text"
                     id="feEmail"
                     placeholder="Location"
                     value={this.state.location} onChange={this.handleLocationChange}
@@ -66,19 +116,42 @@ export default class AddLocation extends Component {
                   />
                 </Col>
                 {/* Password */}
+                <Col md="6" className="form-group" style={{"marginTop":"29px","textAlign":"center"}}>
+                <Button
+                type="button"
+                onClick={this.handleAddSocial}
+                className="small"
+                >
+                Add Slot
+                </Button>
+                </Col>
+              </Row>
+              {this.state.SlotsData.length > 0 && this.state.SlotsData.map((slot,idx) => (
+              <Row key={idx} className="Social">
                 <Col md="6" className="form-group">
-                  <label htmlFor="fePassword">No Of Slots</label>
+                <label htmlFor={`feStartTime${idx}`}>Start Time</label>
                   <FormInput
-                    type="number"
-                    id="fePassword"
-                    placeholder="slots"
-                    pattern="[0-9]*"
-                    value={this.state.slots} onChange={this.handleSlotChange}
-                    autoComplete="current-password"
+                    type="text"
+                    id={`feStartTime${idx}`}
+                    value={slot.startTime}
+                    name={`start_${idx}`}
+                    onChange={ (ev) => {this.handleStartChange(ev,idx) }}
+                  />
+                </Col>
+                <Col md="6" className="form-group">
+                <label htmlFor={`feEndTime${idx}`}>End Time</label> 
+                <a onClick={this.handleRemoveSlot(idx)} style={{float: "right",cursor: "pointer",position: "relative",left: "-5px"}}><b>x</b></a>
+                  <FormInput
+                    type="text"
+                    id={`feEndTime${idx}`}
+                    value={slot.endTime}
+                    name={`end_${idx}`}
+                    onChange={ (ev) => {this.handleEndChange(ev,idx) }}
                   />
                 </Col>
               </Row>
-              <Button onClick={this.handleLogin}>Update & Save</Button>
+            ))}
+              <Button>Update & Save</Button>
             </Form>
           </Col>
         </Row>
@@ -88,15 +161,4 @@ export default class AddLocation extends Component {
 )
   }
 }
-
-// UserAccountDetails.propTypes = {
-//   /**
-//    * The component's title.
-//    */
-//   title: PropTypes.string
-// };
-
-// UserAccountDetails.defaultProps = {
-//   title: "Account Details"
-// };
 
